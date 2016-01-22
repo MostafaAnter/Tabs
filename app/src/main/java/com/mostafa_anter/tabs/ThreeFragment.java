fdf2032d71bf26229bbe60beb9658c9bdc50ee60
Mostafa Anter <ga30000@gmail.com>
2016-01-22 22:11:22 +0200

    final version

---
+++ app/src/main/java/com/mostafa_anter/tabs/ThreeFragment.java
@@ -0,0 +1,27 @@
+package com.mostafa_anter.tabs;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by mostafa on 22/01/16.
+ */
+public class ThreeFragment extends Fragment {
+
+    public ThreeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_three, container, false);
+    }
+}
